31c14fe697b962a093aba36f53e4f08f83cb5cd0
Eugene Mukhin <eug.muhin@gmail.com>
2026-03-17 21:51:33 +0200

    refactor: update IMovie to Movie type and improve code formatting in movieService

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,38 +1,36 @@
-import type { IMovie } from './../types/movie';
-import axios from "axios"
-import toast from "react-hot-toast";
+import type { Movie } from './../types/movie';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 interface MovieHttpResponse {
-    results: IMovie[]
-    total_pages: number
+  results: Movie[];
+  total_pages: number;
 }
 
 interface IFetchMovies {
-    query: string,
-    page: number
+  query: string;
+  page: number;
 }
 
-export default async function fetchMovies({query, page}:IFetchMovies) {
-    const myKey = import.meta.env.VITE_TMDB_TOKEN;
-    const url = `https://api.themoviedb.org/3/search/movie`;
+export default async function fetchMovies({ query, page }: IFetchMovies) {
+  const myKey = import.meta.env.VITE_TMDB_TOKEN;
+  const url = `https://api.themoviedb.org/3/search/movie`;
 
-    const options = {
-        params: {
-            query,
-            page
-        },
-        headers: {
-            Authorization: `Bearer ${myKey}`,
-        }
-    };
+  const options = {
+    params: {
+      query,
+      page,
+    },
+    headers: {
+      Authorization: `Bearer ${myKey}`,
+    },
+  };
 
-    const response = await axios.get<MovieHttpResponse>(url, options);
+  const response = await axios.get<MovieHttpResponse>(url, options);
 
-    if (response.data.results.length === 0) {
-        toast.error('No movies found for your request.');
-    }
+  if (response.data.results.length === 0) {
+    toast.error('No movies found for your request.');
+  }
 
-    return (
-        response.data
-    )
+  return response.data;
 }
